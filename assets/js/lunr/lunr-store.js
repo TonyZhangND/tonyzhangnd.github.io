1cba58334e2340aa069ea2cf8fa80282dbaa8e19
Travis CI <nz97@cornell.edu>
2018-06-26 04:41:31 +0000

    Lastest site built on successful travis build 65 auto-pushed to github

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,26 @@
+var store = [{
+        "title": "My Summer Internship and Writing My First Django App",
+        "excerpt":"My Summer Position This summer, I’m doing a research internship in sunny Singapore. In particular, I’m working at the Institute of High Performance Computing (IHPC), a research institute under the parent company that is the Agency for Science, Technology and Research, known as A*STAR. To many who are probably unfamiliar,...","categories": [],
+        "tags": [],
+        "url": "https://tonyzhangnd.github.io/2017/06/My-Summer-Internship-and-Writing-My-First-Django-App.html",
+        "teaser":"https://tonyzhangnd.github.io/assets/images/my-summer-internship/django-logo-negative.png"},{
+        "title": "Mitosis Image Processing Part 1 - Template Matching Using OpenCV",
+        "excerpt":"Table of Contents Today’s Awesome Problem I Have No Clue, But That’s Awesome Writing the Algorithm Conclusion Complete Code List of references After my first task of developing the Django web application at my summer internship, my second project, to my great excitement, is a lot more algorithmically involved. It...","categories": [],
+        "tags": [],
+        "url": "https://tonyzhangnd.github.io/2017/07/Mitosis-Image-Processing-1.html",
+        "teaser":"https://tonyzhangnd.github.io/assets/images/template-matching/header-tall.png"},{
+        "title": "Mitosis Image Processing Part 2 - Locating Final Points of Interest",
+        "excerpt":"Table of Contents The Input Data Geometric Transformation and Coalescing Algorithm Transform Coalesce Putting It All Together Conclusion and Takeaway My Takeaway Complete Code The problem we are trying to solve is find the exact locations of cells undergoing mitosis in histology images. These are the cells that are annotated...","categories": [],
+        "tags": [],
+        "url": "https://tonyzhangnd.github.io/2017/07/Mitosis-Image-Processing-2.html",
+        "teaser":"https://tonyzhangnd.github.io/assets/images/template-matching/geometry.png"},{
+        "title": "Website Blocker Script - The End of Procrastination",
+        "excerpt":"Like most people, I too am guilty of opening my browser to look up some work related thing on the internet, but somehow find myself half an hour later watching cat videos on YouTube, not remembering what I wanted to do in the first place. Well, such times will be...","categories": [],
+        "tags": [],
+        "url": "https://tonyzhangnd.github.io/2017/12/Website-Blocker.html",
+        "teaser":"https://tonyzhangnd.github.io/assets/images/cat_using_com.jpg"},{
+        "title": "Consensus Protocol Using TLA+",
+        "excerpt":"Table of Contents Consensus TLA+ Model State Machines and Mathematical Functions Init Next Checking for Correctness Concluding Remarks Looking to dive into distributed systems research, I recently started learning how to write TLA+ specifications. TLA+ is a specification language used to mathematically describe an algorithm, and is adopted in both...","categories": [],
+        "tags": [],
+        "url": "https://tonyzhangnd.github.io/2018/01/Consensus-TLA-Spec.html",
+        "teaser":"https://tonyzhangnd.github.io/assets/images/consensus/logo.png"}]
