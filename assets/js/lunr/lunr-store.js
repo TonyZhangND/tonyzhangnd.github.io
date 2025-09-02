32a9f32012a425958dfdaea318715c74c1c21759
Tony Zhang <nz97@cornell.edu>
2025-09-01 21:40:38 -0700

    Deploy: 2025-09-01 21:40:38

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Consensus Protocol Using TLA+",
+        "excerpt":"Table of Contents Consensus TLA+ Model State Machines and Mathematical Functions Init Next Checking for Correctness Concluding Remarks Looking to dive into distributed systems research, I recently started learning how to write TLA+ specifications. TLA+ is a specification language used to mathematically describe an algorithm, and is adopted in both...","categories": [],
+        "tags": [],
+        "url": "https://tonyzhangnd.github.io/2018/01/Consensus-TLA-Spec.html",
+        "teaser":"https://tonyzhangnd.github.io/assets/images/consensus/logo.png"},{
+        "title": "Is AI the Catalyst for Practical Software Verification?",
+        "excerpt":"Formal verification for software systems has been a hot topic in systems research over the past decade. The last two OSDI conferences have yielded four best-paper awards for work applying formal verification to real systems. The reason for this popularity is clear. Verification lets developers write programs that are bug-free...","categories": [],
+        "tags": ["verification","formal methods","artificial intelligence"],
+        "url": "https://tonyzhangnd.github.io/2025/08/AI-for-verification.html",
+        "teaser":null}]
