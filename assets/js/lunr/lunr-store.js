775b7cd4c2a941039f2efd60d6bde5e13705b58e
Tony Zhang <nz97@cornell.edu>
2025-10-28 19:07:39 -0700

    Deploy: 2025-10-28 19:07:39

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Consensus Protocol Using TLA+",
+        "excerpt":"Table of Contents Consensus TLA+ Model State Machines and Mathematical Functions Init Next Checking for Correctness Concluding Remarks Looking to dive into distributed systems research, I recently started learning how to write TLA+ specifications. TLA+ is a specification language used to mathematically describe an algorithm, and is adopted in both...","categories": [],
+        "tags": [],
+        "url": "https://tonyzhangnd.github.io/2018/01/Consensus-TLA-Spec.html",
+        "teaser":"https://tonyzhangnd.github.io/assets/images/consensus/logo.png"},{
+        "title": "Is AI the Catalyst for Practical Software Verification?",
+        "excerpt":"Formal verification for software systems has been a hot topic in systems research over the past decade. The last two OSDI conferences have yielded four best-paper awards for work applying formal verification to real systems. The reason for this popularity is clear. Verification lets developers write programs that are bug-free...","categories": [],
+        "tags": ["verification","formal methods","artificial intelligence"],
+        "url": "https://tonyzhangnd.github.io/2025/08/AI-for-verification.html",
+        "teaser":null},{
+        "title": "(Verifiable) AI is a Compiler",
+        "excerpt":"Modern computers are magic rocks, and programming is the art of making magic rocks do useful things. The challenge is that magic rocks only understand magic rock language, i.e., machine code such as x86 or ARM. Magic rock language is far too cryptic for humans, so clever humans invented high-level...","categories": [],
+        "tags": ["verification","formal methods","artificial intelligence"],
+        "url": "https://tonyzhangnd.github.io/2025/10/AI-is-a-compiler.html",
+        "teaser":null}]
